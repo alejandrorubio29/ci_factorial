39d846014bfa31f08ca88df56828379dea6914d8
alejandrorubio29 <145864071+alejandrorubio29@users.noreply.github.com>
2024-02-16 22:33:54 +0100

    update funcions tras primer test

--- src/functions.py
+++ src/functions.py
@@ -1,6 +1,20 @@
+"""Módulo que contiene la función factorial"""
 
 
 def factorial(num):
+    
+    """
+   Función que devuelve el factorial de un número:
+
+
+   Entrada:
+   - entero: número entero mayor o igual a cero
+
+
+   Salida:
+   - entero: factorial de dicho número
+   """
+
     
     # El factorial d'1 és 1 -> refactorizacion
     if num == 1:
@@ -14,5 +28,5 @@ def factorial(num):
     if num < 0:
         raise ValueError("El argumento debe ser un entero no negativo o cero")
     # El factorial de 5 és 120 -> refactorizacion. Es la formula general del factorial
-    else:
-        return num * factorial(num - 1) # Es una funcion recursiva, se llama a si misma
+    
+    return num * factorial(num - 1) # Es una funcion recursiva, se llama a si misma
