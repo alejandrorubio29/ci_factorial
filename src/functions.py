348b307ff99247b2c088c8c59339a387df86b7e6
alejandrorubio29 <145864071+alejandrorubio29@users.noreply.github.com>
2024-02-16 22:41:54 +0100

    2a mejora

--- src/functions.py
+++ src/functions.py
@@ -2,7 +2,6 @@
 
 
 def factorial(num):
-    
     """
    Función que devuelve el factorial de un número:
 
@@ -15,18 +14,13 @@ def factorial(num):
    - entero: factorial de dicho número
    """
 
-    
-    # El factorial d'1 és 1 -> refactorizacion
     if num == 1:
         return 1
     
-    # Llança una excepció TypeError en no rebre un enter -> refactorizacion
     if not isinstance(num, int):
         raise TypeError("El argumento debe ser un entero")
-    
-    # Llança una excepció ValueError en rebre un enter negatiu -> refactorizacion
+   
     if num < 0:
         raise ValueError("El argumento debe ser un entero no negativo o cero")
-    # El factorial de 5 és 120 -> refactorizacion. Es la formula general del factorial
     
-    return num * factorial(num - 1) # Es una funcion recursiva, se llama a si misma
+    return num * factorial(num - 1) 
